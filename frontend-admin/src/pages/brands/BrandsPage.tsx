6615d464a50877d65218557113af5055e8b6c6c7
tan950 <buivantan28102005@gmail.com>
2026-09-24 16:24:32 +0700

    feat(admin): scaffold frontend-admin/src/pages/brands/BrandsPage.tsx

---
+++ frontend-admin/src/pages/brands/BrandsPage.tsx
@@ -0,0 +1,10 @@
+import PagePlaceholder from '../../components/common/PagePlaceholder'
+
+export default function BrandsPage() {
+  return (
+    <PagePlaceholder
+      title="Quản lý thương hiệu"
+      description="Sprint 1: Table, tìm kiếm, phân trang và modal CRUD thương hiệu."
+    />
+  )
+}
